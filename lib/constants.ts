d7ec495f239ca5667f29645a5934b82d55f2ae43
NINOVEE <109622508+NinoVee@users.noreply.github.com>
2025-12-24 15:40:19 -0800

    Fix import statement formatting in constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,5 @@
 import { PublicKey } from "@solana/web3.js";
 
+// Mint addresses
 export const ACRES_MINT = new PublicKey("9hTF4azRpZQFqgZ3YpgACD3aSbbB4NkeEUhp7NKZvmWe");
 export const WSOL_MINT = "So11111111111111111111111111111111111111112";
